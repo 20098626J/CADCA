0b6f5a50d7f3a341a7756690e9319daeff6c135e
John O'Mahony <07jmahony@gmail.com>
2026-07-20 15:33:18 +0100

    Initial commit

---
+++ test/movie-cast-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MovieCastApi from '../lib/movie-cast-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/movie-cast-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MovieCastApi.MovieCastApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
